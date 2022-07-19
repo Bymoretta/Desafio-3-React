561cfbfd2b1d8ac913defc790da3482aa3baaad5
Axel Moretta <axelmoretta22@gmail.com>
2022-07-18 23:31:43 -0300

    cambio2

--- components/Navbar.js
+++ components/Navbar.js
@@ -11,11 +11,11 @@ export default function ButtonAppBar() {
         <Box sx={{ flexGrow: 12 }}>
             <AppBar  color="" position="fixed">
                 
-                <Toolbar color> Nike
+                <Toolbar color> Code Academy
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         
                     </Typography>
-                    <Button color="inherit" href="https://zapatillas.mercadolibre.com.ar/calzado/nike/?matt_tool=77571335&matt_word=&matt_source=google&matt_campaign_id=1682773904&matt_ad_group_id=65529317436&matt_match_type=&matt_network=g&matt_device=c&matt_creative=399698333235&matt_keyword=&matt_ad_position=&matt_ad_type=&matt_merchant_id=&matt_product_id=&matt_product_partition_id=&matt_target_id=aud-1660898565530:dsa-1466986707538&gclid=Cj0KCQjwidSWBhDdARIsAIoTVb2bJfvQt8ouvQp7tRH1lAzd_aIk2a1UhAfk96VV1TpuRy0frzywe40aAug7EALw_wcB"> Modelos </Button>
+                    <Button color="inherit" href="https://zapatillas.mercadolibre.com.ar/calzado/nike/?matt_tool=77571335&matt_word=&matt_source=google&matt_campaign_id=1682773904&matt_ad_group_id=65529317436&matt_match_type=&matt_network=g&matt_device=c&matt_creative=399698333235&matt_keyword=&matt_ad_position=&matt_ad_type=&matt_merchant_id=&matt_product_id=&matt_product_partition_id=&matt_target_id=aud-1660898565530:dsa-1466986707538&gclid=Cj0KCQjwidSWBhDdARIsAIoTVb2bJfvQt8ouvQp7tRH1lAzd_aIk2a1UhAfk96VV1TpuRy0frzywe40aAug7EALw_wcB"> Ingresar </Button>
                 </Toolbar>
             </AppBar>
         </Box>
